d47ff6b3ca868b048867979c58e5ea3d85a10077
jim0409 <berserker.01.tw@hotmail.com>
2022-04-16 19:16:02 +0800

    misc: modify background

--- src/App.vue
+++ src/App.vue
@@ -15,35 +15,17 @@
   <div id="demo" :class="[{ collapsed: collapsed }, { onmobile: isOnMobile }]">
     <div class="demo">
       <div class="container">
-        <!-- <h1>
-          vue-sidebar-menu
-          <a
-            style="color: #000;text-transform: uppercase;font-size: 14px;font-weight: 400;"
-            href="https://github.com/yaminncco/vue-sidebar-menu"
-          >
-            Github
-          </a>
-        </h1> -->
-        <div>
-          <div>
-            <h1 style="color:white"> Hirzan </h1>
-            <!-- <p>hizran@mail.com</p> -->
-            <h3 style="text-align:right;color:white;">Born for Speed</h3>
-          </div>
-          <div style="text-align:right;color:white;"><a href="https://line.me/R/ti/p/%40pnl5524m">message</a></div>
-        </div>
-        <div>
-          <!-- Select theme:
-          <select v-model="selectedTheme">
-            <option
-              v-for="(theme, index) in themes"
-              :key="index"
-              :value="theme.input"
+        <h1 style="color:white">
+          Hizran
+          <div style="text-align:right;">
+            <a
+              style="color: red;text-transform: uppercase;font-size: 14px;font-weight: 400"
+              href="https://line.me/R/ti/p/%40pnl5524m"
             >
-              {{ theme.name }}
-            </option>
-          </select> -->
-        </div>
+              Born for speed
+            </a>
+          </div>
+        </h1>
         <hr style="margin: 50px 0px; border: 1px solid #e3e3e3" />
         <router-view />
       </div>
@@ -168,10 +150,12 @@ body {
   font-size: 18px;
   background-color: #f2f4f7;
   background-image: url("../public/rider.jpeg");
-  background-size: cover;
+  background-position: center;
   /* background-size: auto; */
+  /* background-size: 100%;  */
+  /* background-size: cover; */
+  background-size:contain;
   background-repeat:no-repeat;
-  background-size: 100%; 
   background-attachment: fixed;
   border:1px #ccc solid;
   color: #262626;
